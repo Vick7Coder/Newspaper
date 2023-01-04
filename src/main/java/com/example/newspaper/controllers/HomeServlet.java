701ecb77e7b291a736b42faff41348317a0b11c1
Vick7 <phamhieu16082002@gmail.com>
2023-01-04 17:18:24 +0700

    first commit

---
+++ src/main/java/com/example/newspaper/controllers/HomeServlet.java
@@ -0,0 +1,43 @@
+package com.example.newspaper.controllers;
+
+import com.ute.newsproject.utils.ServletUtils;
+import com.ute.newsproject.utils.ServletUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", value = "/Home/*")
+public class HomeServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String path = request.getPathInfo();
+    if (path == null || path.equals("/")) {
+      path = "/Index";
+    }
+
+    switch (path) {
+      case "/Index":
+        // HttpSession session = request.getSession();
+        // System.out.println(session.getAttribute("auth"));
+        // System.out.println(session.getAttribute("authUser"));
+
+        ServletUtils.forward("/views/vwHome/Index.jsp", request, response);
+        break;
+      case "/About":
+        ServletUtils.forward("/views/vwHome/About.jsp", request, response);
+        break;
+      default:
+        ServletUtils.forward("/views/404.jsp", request, response);
+        break;
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
+}
---
+++ src/main/java/com/example/newspaper/service/CategoryService.java
@@ -0,0 +1,30 @@
+package com.example.newspaper.service;
+
+import com.ute.newsproject.beans.Category;
+import com.ute.newsproject.utils.DbUtils;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class CategoryService {
+    public static List<Category> getAll() {
+        String sql = "select * from categories";
+        try (Connection con = DbUtils.getConnection()) {
+            return con.createQuery(sql).executeAndFetch(Category.class);
+        }
+    }
+
+    public static Category get(int id) {
+        String sql = "select * from categories where cat_id = :cat_id";
+        try (Connection con = DbUtils.getConnection()) {
+            List<Category> list = con.createQuery(sql)
+                    .addParameter("cat_id", id)
+                    .executeAndFetch(Category.class);
+
+            if (list.size() == 0)
+                return null;
+
+            return list.get(0);
+        }
+    }
+}
